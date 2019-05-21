3e719020d2ced3d22b7918e19558dbd724a4de38
Sujeong Park <s2017s08@e-mirim.hs.kr>
2019-05-21 10:44:15 +0900

    #1 하단에 라벨 추가

--- TeaBagMaker/Form1.Designer.cs
+++ TeaBagMaker/Form1.Designer.cs
@@ -28,9 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.teaList = new System.Windows.Forms.ComboBox();
             this.btn = new System.Windows.Forms.Button();
             this.result = new System.Windows.Forms.Label();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // teaList
@@ -41,6 +44,7 @@
             this.teaList.Name = "teaList";
             this.teaList.Size = new System.Drawing.Size(204, 20);
             this.teaList.TabIndex = 0;
+            this.teaList.SelectedIndexChanged += new System.EventHandler(this.TeaList_SelectedIndexChanged);
             // 
             // btn
             // 
@@ -50,6 +54,7 @@
             this.btn.TabIndex = 1;
             this.btn.Text = "담그기!";
             this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.Btn_Click);
             // 
             // result
             // 
@@ -59,17 +64,34 @@
             this.result.Size = new System.Drawing.Size(0, 12);
             this.result.TabIndex = 2;
             // 
+            // Timer
+            // 
+            this.Timer.Interval = 1000;
+            this.Timer.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(28, 263);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "2019-05-21   3607 박수정";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(237, 288);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.result);
             this.Controls.Add(this.btn);
             this.Controls.Add(this.teaList);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,6 +102,8 @@
         private System.Windows.Forms.ComboBox teaList;
         private System.Windows.Forms.Button btn;
         private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- TeaBagMaker/Form1.cs
+++ TeaBagMaker/Form1.cs
@@ -12,9 +12,40 @@ namespace TeaBagMaker
 {
     public partial class Form1 : Form
     {
+        string[] tList = new string[] { "홍차", "녹차", "루이보스차", "국화차" };
+       
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int i=0;i<tList.Length;i++)
+            {
+                this.teaList.Items.Add(tList[i]);
+            }
+            if(teaList.Items.Count >0)
+            {
+                this.teaList.SelectedIndex = 0;
+            }
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TeaList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(this.teaList.Text != "")
+            {
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
 
         }
     }
